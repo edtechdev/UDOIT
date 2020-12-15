67875e4d271bb62379a728d73904846a0264284f
Doug Holton <65940+edtechdev@users.noreply.github.com>
2020-12-15 11:22:45 -0500

    adding support info to welcome message

--- config/settings.php
+++ config/settings.php
@@ -106,6 +106,8 @@ header("Pragma: no-cache"); // HTTP 1.0.
 header("Expires: 0"); // Proxies.
 
 // Messages
-$udoit_welcome_message = 'The Universal Design Online content Inspection Tool (U<strong>DO</strong>IT) was created by the Center for Distributed Learning at the University of Central Florida. U<strong>DO</strong>IT will scan your course content, generate a report and provide instructions on how to correct accessibility issues. Funding for U<strong>DO</strong>IT was provided by a Canvas Grant awarded in 2014.';
+
+            
+$udoit_welcome_message = '<strong>Please contact Doug Holton or Brennen Cannon for assistance with <a href="https://training.it.ufl.edu/training/items/udoit.html" target="_blank">using UDOIT</a> or <a href="https://community.canvaslms.com/t5/Accessibility/How-to-Fix-and-Prevent-Accessibility-Issues-in-Your-Canvas/ba-p/274000" target="_blank">fixing accessibility issues.</a></strong> <br /><br />The Universal Design Online content Inspection Tool (U<strong>DO</strong>IT) was created by the Center for Distributed Learning at the University of Central Florida. U<strong>DO</strong>IT will scan your course content, generate a report and provide instructions on how to correct accessibility issues.';
 
 $udoit_disclaimer_message = 'Please Note: This tool is meant to be used as a guide, not a certification. It only checks for common accessibility issues, and is not comprehensive; a clean report in U<strong>DO</strong>IT does not necessarily mean that your course is fully accessible. Likewise, the tool may indicate a possible accessibility issue where one does not exist.';
